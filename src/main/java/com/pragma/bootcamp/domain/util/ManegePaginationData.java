a242d3fadaad5563d0561bd4d1de936238ac927b
Victor Agudelo <viactur11@gmail.com>
2024-03-16 11:18:45 -0500

    Se refactorizo la clase que administra los datos para la paginación, con el fin de poder usarla mas adelante

--- src/main/java/com/pragma/bootcamp/domain/util/ManegePaginationData.java
+++ src/main/java/com/pragma/bootcamp/domain/util/ManegePaginationData.java
@@ -8,6 +8,7 @@ public final class ManegePaginationData {
   private static final Integer DEFAULT_PAGE = 0;
   private static final Integer DEFAULT_SIZE = 10;
   private static final Direction DEFAULT_DIRECTION = Direction.ASC;
+  public static final String DEFAULT_ORDER = "name";
 
   private ManegePaginationData() { throw new IllegalStateException("utility class"); }
 
@@ -15,13 +16,19 @@ public final class ManegePaginationData {
     DESC,
     ASC
   }
+
   public static PaginationData definePaginationData(Integer page, Integer size, String order) {
+    return definePaginationData(page, size, order, DEFAULT_ORDER);
+  }
+
+  public static PaginationData definePaginationData(Integer page, Integer size, String order, String orderBy) {
 
     var temporaryPage = Objects.isNull(page) ? DEFAULT_PAGE : page;
     var temporarySize = Objects.isNull(size)  ? DEFAULT_SIZE : size;
     var temporaryOrder = Objects.isNull(order)  ? DEFAULT_DIRECTION : validateDirection(order);
+    var temporaryOrderBy = Objects.isNull(orderBy) ? DEFAULT_ORDER : orderBy;
 
-    return new PaginationData(temporaryPage, temporarySize, temporaryOrder.name(), "name");
+    return new PaginationData(temporaryPage, temporarySize, temporaryOrder.name(), temporaryOrderBy);
   }
 
   private static Direction validateDirection(String order) {
