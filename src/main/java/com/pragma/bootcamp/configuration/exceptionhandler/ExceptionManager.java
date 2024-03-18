a965e26ab049c29f72054efd5f58d0c44142fbac
Victor Agudelo <viactur11@gmail.com>
2024-03-18 16:06:49 -0500

    se agrego al ExceptionManager la validación para el tipo de dato incorrecto

--- src/main/java/com/pragma/bootcamp/configuration/exceptionhandler/ExceptionManager.java
+++ src/main/java/com/pragma/bootcamp/configuration/exceptionhandler/ExceptionManager.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -53,6 +54,13 @@ public class ExceptionManager {
         .toList();
 
     return ResponseEntity.badRequest().body(errors);
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ExceptionArgumentResponse> handlerTypeInvalid(MethodArgumentTypeMismatchException exception) {
+    return ResponseEntity.badRequest().body(
+        new ExceptionArgumentResponse(exception.getName(), exception.getMessage())
+    );
   }
 
   @ExceptionHandler(NoDataFoundException.class)
