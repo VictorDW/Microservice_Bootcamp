639a71449d8bd18c1f1c0a96844f91af1885ee0c
Victor Agudelo <viactur11@gmail.com>
2024-03-21 10:38:22 -0500

    Se implemento el primer test en el Controller del adaptador Rest para la primera HU

---
+++ src/test/java/com/pragma/bootcamp/adapters/driving/http/controller/TechnologyControllerTest.java
@@ -0,0 +1,64 @@
+package com.pragma.bootcamp.adapters.driving.http.controller;
+
+import com.pragma.bootcamp.adapters.driving.http.adapter.ITechnologyServiceAdapter;
+import com.pragma.bootcamp.adapters.driving.http.dto.request.AddTechnologyRequest;
+import com.pragma.bootcamp.adapters.driving.http.dto.response.TechnologyResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class TechnologyControllerTest {
+    @Mock
+    private ITechnologyServiceAdapter technologyHandler;
+    @InjectMocks
+    private TechnologyController restController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(restController).build();
+    }
+
+    @DisplayName("Given an http request you should create a technology")
+    @Test
+    void test1() throws Exception {
+
+        TechnologyResponse response = new TechnologyResponse(1L,"Java","Java con versión JDK 17");
+
+        given(technologyHandler.createTechnology(any(AddTechnologyRequest.class))).willReturn(response);
+
+        String bodyRequest = "{\"name\":\"Python\",\"description\":\"Java con versión JDK 17\"}";
+
+        MockHttpServletRequestBuilder requestBuilder = post("/api/technology")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bodyRequest);
+
+        //GIVEN
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Java"));
+
+        //WHEN - THEN
+    }
+}
