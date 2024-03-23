6225e3d25edda9e4d2de33543928b1e06a50fc3d
Victor Agudelo <viactur11@gmail.com>
2024-03-23 13:16:56 -0500

    se agrego el primer test al adaptador de percitencia de la capacidad

---
+++ src/test/java/com/pragma/bootcamp/adapters/driven/jpa/mysql/adapter/CapacityPersistenceAdapterTest.java
@@ -0,0 +1,56 @@
+package com.pragma.bootcamp.adapters.driven.jpa.mysql.adapter;
+
+import com.pragma.bootcamp.adapters.driven.jpa.mysql.entity.CapacityEntity;
+import com.pragma.bootcamp.adapters.driven.jpa.mysql.exception.NotFoundException;
+import com.pragma.bootcamp.adapters.driven.jpa.mysql.mapper.ICapacityEntityMapper;
+import com.pragma.bootcamp.adapters.driven.jpa.mysql.repository.ICapacityRepository;
+import com.pragma.bootcamp.adapters.driven.jpa.mysql.repository.ITechnologyRepository;
+import com.pragma.bootcamp.domain.model.Capacity;
+import com.pragma.bootcamp.domain.model.Technology;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CapacityPersistenceAdapterTest {
+
+  @InjectMocks
+  private CapacityPersistenceAdapter capacityPersistenceAdapter;
+  @Mock
+  private  ICapacityEntityMapper capacityEntityMapper;
+  @Mock
+  private  ITechnologyRepository technologyRepository;
+  @Mock
+  private  ICapacityRepository capacityRepository;
+
+  @Test
+  @DisplayName("should throw an exception when trying to create a capacity with a non-existing technology")
+  void test1() {
+
+    //GIVEN
+    List<Technology> technologies = List.of(
+        new Technology(null, "Java", null),
+        new Technology(null, "Python", null),
+        new Technology(null, "Javascript", null)
+    );
+
+    CapacityEntity capacityEntity = new CapacityEntity();
+
+    Capacity givenCapacity = new Capacity(1L, "Backend Java", "Java Backend Developer", technologies);
+    given(capacityEntityMapper.modelToEntity(givenCapacity)).willReturn(capacityEntity);
+    given(technologyRepository.findByNameIgnoreCase(technologies.get(0).getName())).willReturn(Optional.empty());
+
+    //WHEN - THAT
+    assertThrows(NotFoundException.class, ()-> capacityPersistenceAdapter.saveCapacity(givenCapacity));
+
+  }
+}
