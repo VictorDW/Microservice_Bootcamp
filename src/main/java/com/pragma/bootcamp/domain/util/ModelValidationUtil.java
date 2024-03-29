16361dbba3948a70701c4d19b40edfb9e9175e2f
Victor Agudelo <viactur11@gmail.com>
2024-03-29 02:19:10 -0500

    Se implemento una clase estatica que permite reutilizar el codigo para validar los criterios de acceptación de Bootcamp y Capacity

--- src/main/java/com/pragma/bootcamp/domain/util/ModelValidationUtil.java
+++ src/main/java/com/pragma/bootcamp/domain/util/ModelValidationUtil.java
@@ -1,23 +1,28 @@
 package com.pragma.bootcamp.domain.util;
 
 import com.pragma.bootcamp.domain.exception.NumberOutOfRangeException;
-import com.pragma.bootcamp.domain.model.Capacity;
-import com.pragma.bootcamp.domain.model.Technology;
+import com.pragma.bootcamp.domain.exception.RepeatedModelException;
+import com.pragma.bootcamp.domain.model.ParentModel;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ModelValidationUtil {
 
-  public ModelValidationUtil() {
+  private ModelValidationUtil() {
     throw new IllegalStateException("Utility class");
   }
 
-  public static void executeValidationModelRange(List<?> list, String pluralModelName, String parentModelName, Integer min, Integer max) {
+  public static void executeValidationModel(List<? extends ParentModel> list, String pluralModelName, String parentModelName, Integer min, Integer max) {
+    executeValidationModelRange(list, pluralModelName, parentModelName, min, max);
+    executeValidationNameModelRepeated(list, pluralModelName, parentModelName);
+  }
 
-    int technologySize = list.size();
+  private static void executeValidationModelRange(List<? extends ParentModel> list, String pluralModelName, String parentModelName, Integer min, Integer max) {
 
-    if (technologySize < min || technologySize > max) {
+    int size = list.size();
 
+    if (size < min || size > max) {
       throw new NumberOutOfRangeException(
           String.format(
               DomainConstants.NUMBER_RANGE_MESSAGE,
@@ -26,5 +31,20 @@ public class ModelValidationUtil {
               min,
               max));
     }
+  }
+
+  private static void executeValidationNameModelRepeated(List<? extends ParentModel> list, String pluralModelName, String parentModelName ) {
+
+    HashSet<String> uniqueName = new HashSet<>(list.size());
+
+    list.forEach(model -> {
+      if (!uniqueName.add(model.getName()))
+        throw new RepeatedModelException(
+            String.format(
+                DomainConstants.REPEATED_MODEL_MESSAGE,
+                parentModelName,
+                pluralModelName));
+    });
+
   }
 }
